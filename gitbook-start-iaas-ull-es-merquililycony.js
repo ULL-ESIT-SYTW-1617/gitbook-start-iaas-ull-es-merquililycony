60780ac4d71acad06906a5a916c266004dd7c025
Edna Liliana Galiano <alu0100762846@ull.edu.es>
2016-11-01 14:45:21 +0000

    Nuevos cambios.

--- gitbook-start-iaas-ull-es-merquililycony.js
+++ gitbook-start-iaas-ull-es-merquililycony.js
@@ -4,20 +4,40 @@ const exec_ssh = require('ssh-exec');
 const fs = require('fs-extra');
 const path = require('path');
 const gitUrlParse = require("git-url-parse");
-var doc = gitUrlParse()
 
+
+
+
+var reply = ((stdout,stderr,error) =>{
+    if(error){
+        console.error("Se ha producido un error:"+error);
+        console.log("Stdout:"+stdout);
+        console.log("Stderr:"+stderr);
+        
+        
+    }
+});
 
 var deploy = ((ip,user,url,route) => {
     
     var doc = gitUrlParse(url);
-    console.log("Iniciando despliegue en Iaas");
+    console.log("Iniciando despliegue en Iaas...");
     console.log('Direccion IP : '+ip);
     console.log('Usuario : '+user);
     console.log('Url : '+url);
-    // console.log('Ruta : '+route'/'+doc.name)
+    console.log('Ruta : '+route+'/'+doc.name);
     
-    // // exec_ssh('cd')
-   
-    
+    exec_ssh('cd '+route+';git pull '+url+' master',{
+        user: user,
+        host: ip,
+        key: '~/.ssh/iaas.pub'
+    },reply); 
     
 });
+
+
+module.exports = {
+//   initialize,
+  reply,
+  deploy
+}
