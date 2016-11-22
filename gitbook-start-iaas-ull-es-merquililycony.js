ed924f4cee52648cbbbc9cdf3e15f7dcecd9c620
Edna liliana Galiano <alu0100762846@ull.edu.es>
2016-11-22 19:14:51 +0000

    Cambios importantes.

--- gitbook-start-iaas-ull-es-merquililycony.js
+++ gitbook-start-iaas-ull-es-merquililycony.js
@@ -1,114 +1,102 @@
-const exec_ssh = require('ssh-exec');
-const fs = require('fs-extra');
-const path = require('path');
-const gitUrlParse = require("git-url-parse");
-var scp_ = require('scp');//Send a file to a remote host (in your ~/.ssh/config)
 
 
-var reply = ((stdout,stderr,error) => {
-    if(error){
-        console.error("Se ha producido un error:"+error);
-        console.log("Stdout:"+stdout);
-        console.log("Stderr:"+stderr);
-    }
-    
-});
+// const path = require('path');
+// var scp_ = require('scp');//Send a file to a remote host (in your ~/.ssh/config)
 
-
-module.exports = {
-
-    initialize: (ip,user,url,route) =>{// Inicialize del plugin deploy-iaas-ull
+    function initialize (){// Inicialize del plugin deploy-iaas-ull
         
-        
+        var exec_ssh = require('ssh-exec');
         var dir = process.cwd() + '/';
         var r = path.join(__dirname, 'gulpfile.js')//ruta
-        var datos = {
-            user: user,
-            host: ip,
-            file: 'iaas.pub',
-            path: '~/.ssh/'
-        }
-        
+        var dato = require(dir + "package.jason");
+
+        var task = '\ngulp.task("deploy-ull-iaas-es", function () {'+ 
+        '\n\tvar iaas = require("gitbook-start-iaas-ull-es-merquililycony");'+
+        '\n\tvar result = iaas.deploy();'+
+        '\n});\n\n';
+
+
         // Leemos fichero gulpfile que esta dentro del directorio
         
-       fs.readFile(dir + 'gulpfile.js',"utf-8", (err,data) => {
-            if(err)  throw err;
-            
-            if(data.search("deploy-iaas-ull") != -1){//Ya existe la tarea
-                console.log("La tarea ya está creada!!")
-            }else{   //En el caso de que no, lo añade
-            
-                 fs.readFile(r,(err,data) => {
-                    if (err)
-                        throw err;
-                    else{    
-                        fs.appendFile(dir +'gulpfile.js', data, (err) => {
-                            if (err) 
-                                throw err;
-                    
-                        });
-                    }
-                });
-            } 
-        });
-        
-        
-         // Creamos el fichero con la clave publica
-        
-        exec_ssh("ssh-keygen -f iaas");
-        
-        
-        
-        scp_.send(datos, (err) => {
-          if (err) 
-            throw err;
-          else
-          {
-            exec_ssh("ssh-copy-id -i iaas " + user + "@" + ip);//añadiendo clave a fichero 
-            exec_ssh("mv iaas ~/.ssh; mv iaas.pub ~/.ssh",(err) => {
-                if(err){
-                    throw err;
-                }else{
-                    console.log("Tranferencia de archivo iaas.pub a la maquina IAAS realizada correctamente")
-                }
-            
-            }); 
-          }
-        });
-        
-            
-      exec_ssh('cd'+route+ ';git clone' + url+ '',{
-          user: user,
-          host: ip,
-          key: '~/.ssh/iaas.pub'
-        }, reply);
-        
-        
-    },
+       fs.readFile(dir + 'gulpfile.js',"utf-8",function(err,data) {
+            if(err) throw err;
+            if(data.search("deploy-ull-iaas-es") != -1){
+                console.log("Tarea encontrada!")
+            }else{
+                    fs.writeFileSync(path.resolve(process.cwd(),'gulpfile.js'), task,  {'flag':'a'},  function(err) {
+                        if (err) {
+                            return console.error(err);
+                        }
+                    });
+            }
 
 
-
-    deploy: (ip,user,url,route) => {
+       });
+            
         
-        var doc = gitUrlParse(url);
+      //    // Creamos el fichero con la clave publica
         
-        console.log("Iniciando despliegue en Iaas...");
-        console.log('Direccion IP : '+ip);
-        console.log('Usuario : '+user);
-        console.log('Url : '+url);
-        console.log('Ruta : '+route+'/'+doc.name);
+      //   exec_ssh("ssh-keygen -f iaas");
         
         
-        // Hacemos pull del repositorio
-         
-        exec_ssh('cd '+route+'/'+doc.name+';git pull',{
-            user: user,
-            host: ip,
-            key: '~/.ssh/iaas.pub'
-        },reply); 
-       
         
-    }
-
+      //   scp_.send(datos, (err) => {
+      //     if (err) 
+      //       throw err;
+      //     else
+      //     {
+      //       exec_ssh("ssh-copy-id -i iaas " + user + "@" + ip);//añadiendo clave a fichero 
+      //       exec_ssh("mv iaas ~/.ssh; mv iaas.pub ~/.ssh",(err) => {
+      //           if(err){
+      //               throw err;
+      //           }else{
+      //               console.log("Tranferencia de archivo iaas.pub a la maquina IAAS realizada correctamente")
+      //           }
+            
+      //       }); 
+      //     }
+      //   });
+        
+            
+      // exec_ssh('cd'+route+ ';git clone' + url+ '',{
+      //     user: user,
+      //     host: ip,
+      //     key: '~/.ssh/iaas.pub'
+      //   }, reply);
+        
+        
 };
 
+function deploy() {
+        
+
+        console.log("Empezando el despliegue...");
+        
+        var dir = process.cwd() + '/';
+        var dato = require(dir + "package.jason");
+        var exec_ssh = require('ssh-exec');
+        var fs = require('fs-extra');
+        
+            // // Hacemos clone del repositorio
+        exec_ssh('cd '+dato.iaas.ruta+';git clone'+dato.iaas.url+'',{
+            user: dato.iaas.user,
+            host: dato.iaas.ip,
+            key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
+        },function(err){
+            if(err){
+                console.log("Hacemos un pull al repositorio.")
+                exec_ssh('cd '+dato.iaas.ruta+';git pull',{
+                    user: dato.iaas.user,
+                    host: dato.iaas.ip,
+                    key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
+                });
+            }
+
+
+    });
+                  
+};
+
+
+exports.initialize = initialize;
+exports.deploy = deploy;
